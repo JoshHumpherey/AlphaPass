42d279efd4568b6a7e264c992272b3b8de7f0eeb
Noah-Wagner <noah@noah-wagner.com>
2018-05-02 14:38:55 -0400

    Cleaning up some naming and migrating to py.test

--- password_gen.py
+++ password_gen.py
@@ -1,6 +1,7 @@
 from hashlib import sha256
 
-def GeneratePassword(masterPassword, serviceName):
-    securePassword = sha256((masterPassword+serviceName).encode('utf-8')).hexdigest()
-    print(securePassword)
-    return securePassword
+
+def generate_password(master_password, service_name):
+    secure_password = sha256((master_password + service_name).encode('utf-8')).hexdigest()
+    print(secure_password)
+    return secure_password
---
+++ test_password_gen.py
@@ -0,0 +1,9 @@
+import password_gen
+
+
+def test_PasswordGen():
+    expected_result = '749bd32ac64d1ceb519878e6e1925d072b8bf143eb84c035cb94caf9bc693a6f'
+    master_key = '1234'
+    service_name = 'Bank'
+    test_password = password_gen.generate_password(master_key, service_name)
+    assert test_password == expected_result
