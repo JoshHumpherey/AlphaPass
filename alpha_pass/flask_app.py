3cf36b2d51328b2e504ca8f66140acda1428a707
JoshHumpherey <jhumphrey321@gmail.com>
2018-05-03 15:05:01 -0400

    Adding icon, logo, obscuring password, and other improvements

--- alpha_pass/flask_app.py
+++ alpha_pass/flask_app.py
@@ -10,8 +10,8 @@ def hello():
 
 @app.route('/generate_password',methods=['GET'])
 def handle_data():
-    master_password = request.form['master_password']
-    service_name = requeswt.form['service_name']
+    #master_password = request.form['master_password']
+    #service_name = requeswt.form['service_name']
     print(password_gen.generate_password('ABC','DEF'))
 
 if __name__ == "__main__":
